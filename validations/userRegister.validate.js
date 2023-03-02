cf7590532a921946297f1061c4d8cb9a27217f7f
umairshamoon <umairshamoon513@gmail.com>
2023-03-02 13:20:04 +0500

    username removed

--- validations/userRegister.validate.js
+++ validations/userRegister.validate.js
@@ -2,7 +2,7 @@ const Joi = require('joi')
 module.exports = function (admin) {
   const adminSchema = Joi.object({
     email: Joi.string().email().required(),
-    password: Joi.string().min().required(),
+    password: Joi.string().required(),
   })
   return adminSchema.validate(admin)
 }
