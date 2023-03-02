bca7298f34556034871a7cf96e55926854264533
umairshamoon <umairshamoon513@gmail.com>
2023-03-02 16:10:05 +0500

    string

--- validations/item.validation.js
+++ validations/item.validation.js
@@ -1,9 +1,9 @@
 const Joi = require('joi')
 module.exports = function (data) {
   const loginSchema = Joi.object({
-    name: Joi.string().string().required(),
+    name: Joi.string().required(),
     startPrice: Joi.number().min(1).required(),
-    timeWindow: Joi.string().min(5).required(),
+    timeWindow: Joi.number().min(5).required(),
 
   })
   return loginSchema.validate(data)
